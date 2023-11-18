216ab464af57f0677712b12c0bbd3cee2d5b29a5
MariyqZlateva <mimitooo4@gmail.com>
2023-11-18 16:07:11 +0200

    Added Using Profiles

---
+++ spring-6-di/src/main/java/com/zlateva/springframework/spring6di/servises/i18n/EnglishGreetingService.java
@@ -0,0 +1,14 @@
+package com.zlateva.springframework.spring6di.servises.i18n;
+
+import com.zlateva.springframework.spring6di.servises.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18NService")
+public class EnglishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
