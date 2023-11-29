76d1453fd4b108ee52f9247bedfb9d35e3714606
MariyqZlateva <mimitooo4@gmail.com>
2023-11-29 12:24:48 +0200

    Adding list example

--- spring-6-rest-mvc/src/main/java/com/zlateva/spring6restmvc/services/BeerService.java
+++ spring-6-rest-mvc/src/main/java/com/zlateva/spring6restmvc/services/BeerService.java
@@ -2,8 +2,10 @@ package com.zlateva.spring6restmvc.services;
 
 import com.zlateva.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
     Beer getBeerById(UUID id);
 }
