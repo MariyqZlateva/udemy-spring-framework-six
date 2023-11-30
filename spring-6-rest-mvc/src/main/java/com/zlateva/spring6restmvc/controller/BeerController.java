c4293a9f955e4f10f137b02306f33002c9902ed1
MariyqZlateva <mimitooo4@gmail.com>
2023-11-30 16:03:25 +0200

    Adding Http DELETE on beer

--- spring-6-rest-mvc/src/main/java/com/zlateva/spring6restmvc/controller/BeerController.java
+++ spring-6-rest-mvc/src/main/java/com/zlateva/spring6restmvc/controller/BeerController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,6 +24,12 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
+    @DeleteMapping("{beerId}")
+    public ResponseEntity deleteById(@PathVariable("beerId") UUID beerId){
+
+        beerService.deleteById(beerId);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 
     @PutMapping("{beerId}")
     public ResponseEntity updateByID(@PathVariable("beerId")UUID beerId,@RequestBody Beer beer) {
