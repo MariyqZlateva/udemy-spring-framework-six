d6da1639d6f09c11efb0d03f6b0d35d5174bda9f
MariyqZlateva <mimitooo4@gmail.com>
2023-12-05 11:41:50 +0200

    Adding initial configuration of BeerTest MockMVC

--- spring-6-rest-mvc/src/main/java/com/zlateva/spring6restmvc/controller/BeerController.java
+++ spring-6-rest-mvc/src/main/java/com/zlateva/spring6restmvc/controller/BeerController.java
@@ -2,7 +2,7 @@ package com.zlateva.spring6restmvc.controller;
 
 import com.zlateva.spring6restmvc.model.Beer;
 import com.zlateva.spring6restmvc.services.BeerService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
