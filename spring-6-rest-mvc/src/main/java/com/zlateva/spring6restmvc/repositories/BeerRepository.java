7d961e0d0cb124c259e01c352424df18459c1e1e
MariyqZlateva <mimitooo4@gmail.com>
2023-12-07 15:11:14 +0200

    Adding Spring Data JPA Repositories

---
+++ spring-6-rest-mvc/src/main/java/com/zlateva/spring6restmvc/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.zlateva.spring6restmvc.repositories;
+
+import com.zlateva.spring6restmvc.entities.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+}
