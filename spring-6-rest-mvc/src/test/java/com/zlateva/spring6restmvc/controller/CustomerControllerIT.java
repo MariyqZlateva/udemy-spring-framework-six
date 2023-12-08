df0e8afe189bae3049fc8ffbc8e2069824a0a0e5
MariyqZlateva <mimitooo4@gmail.com>
2023-12-08 16:50:09 +0200

    Adding CustomerController integration Test

---
+++ spring-6-rest-mvc/src/test/java/com/zlateva/spring6restmvc/controller/CustomerControllerIT.java
@@ -0,0 +1,59 @@
+package com.zlateva.spring6restmvc.controller;
+
+import com.zlateva.spring6restmvc.entities.Customer;
+import com.zlateva.spring6restmvc.model.CustomerDTO;
+import com.zlateva.spring6restmvc.repositories.CustomerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class CustomerControllerIT {
+    @Autowired
+    CustomerController customerController;
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testCustomerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            customerController.getCustomerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById() {
+        Customer customer = customerRepository.findAll().get(0);
+
+        CustomerDTO dto = customerController.getCustomerById(customer.getId());
+
+        assertThat(dto).isNotNull();
+
+    }
+
+    @Test
+    void testListCustomers() {
+        List<CustomerDTO> dtos = customerController.listAllCustomer();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        customerRepository.deleteAll();
+        List<CustomerDTO> dtos = customerController.listAllCustomer();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+
+}
