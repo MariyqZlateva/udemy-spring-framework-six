8feebeb1e4c654ea0b4ef1f683003f3853c1c101
MariyqZlateva <mimitooo4@gmail.com>
2024-02-22 09:47:37 +0200

    downed spring version

--- spring-6-rest-mvc/src/main/java/com/zlateva/spring6restmvc/config/SpringSecConfig.java
+++ spring-6-rest-mvc/src/main/java/com/zlateva/spring6restmvc/config/SpringSecConfig.java
@@ -2,14 +2,20 @@ package com.zlateva.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().ignoringRequestMatchers("/api/**");
+        http.authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic(Customizer.withDefaults())
+                .csrf().ignoringRequestMatchers("/api/**");
         return http.build();
     }
 }
