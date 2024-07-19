244d8be4ac0715bb2bd48809afcbc981aaf7e9ed
MariyqZlateva <mimitooo4@gmail.com>
2024-07-19 17:01:46 +0300

    refactoring SpringSecConfig

--- spring-6-rest-mvc/src/main/java/com/zlateva/spring6restmvc/config/SpringSecConfig.java
+++ spring-6-rest-mvc/src/main/java/com/zlateva/spring6restmvc/config/SpringSecConfig.java
@@ -2,6 +2,7 @@ package com.zlateva.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,12 +10,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SpringSecConfig {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf(AbstractHttpConfigurer::disable);
+//        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+//        http.oauth2ResourceServer(Customizer.withDefaults());
+//        http.oauth2Client(Customizer.withDefaults());
+//        return http.build();
+//    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .anyRequest().authenticated()
-                .and().oauth2ResourceServer().jwt();
+        http.authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();
+                })
+                .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
+                    httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());
+                });
 
         return http.build();
     }
+
 }
