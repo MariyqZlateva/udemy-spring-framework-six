cdcfaf746801a8bb8571576e67b10b579dfb69a0
MariyqZlateva <mimitooo4@gmail.com>
2024-08-16 11:35:11 +0300

    adding mono examples

--- spring-6-reactive-examples/src/main/java/com/zlateva/spring6reactiveexamples/repositories/PersonRepositoryImpl.java
+++ spring-6-reactive-examples/src/main/java/com/zlateva/spring6reactiveexamples/repositories/PersonRepositoryImpl.java
@@ -9,7 +9,7 @@ public class PersonRepositoryImpl implements PersonRepository {
     Person michel = Person.builder().id(1).firstName("Michel").lastName("Weston").build();
     Person fiona = Person.builder().id(2).firstName("Fiona").lastName("Glenanne").build();
     Person sam = Person.builder().id(3).firstName("Sam").lastName("Axe").build();
-    Person jesse = Person.builder().id(1).firstName("Jesse").lastName("Porter").build();
+    Person jesse = Person.builder().id(4).firstName("Jesse").lastName("Porter").build();
 
     @Override
     public Mono<Person> getById(Integer id) {
