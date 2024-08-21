63bc146cb9308bff0256eb5c9147f51d42fb7c77
MariyqZlateva <mimitooo4@gmail.com>
2024-08-21 10:57:33 +0300

    adding fined by id not found

--- spring-6-reactive-examples/src/test/java/com/zlateva/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
+++ spring-6-reactive-examples/src/test/java/com/zlateva/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -101,4 +101,25 @@ class PersonRepositoryImplTest {
 
         fionaMono.subscribe(person -> System.out.println(person.getFirstName()));
     }
+
+    @Test
+    void testFindPersonByIdNotFound() {
+
+        Flux<Person> personFlux = personRepository.finedAll();
+
+        final  Integer id = 8;
+
+        Mono<Person> personMono = personFlux.filter(person -> person.getId().equals(id)).single()
+                .doOnError(throwable -> {
+                    System.out.println("Error occurred in the flux");
+                    System.out.println(throwable.toString());
+                });
+
+        personMono.subscribe(person -> {
+            System.out.println(person.toString());
+        }, throwable -> {
+            System.out.println("Error occurred in the mono");
+            System.out.println(throwable.toString());
+        });
+    }
 }
