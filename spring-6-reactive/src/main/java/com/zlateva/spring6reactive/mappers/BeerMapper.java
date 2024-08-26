37339945437896a27483f00aa66bce3c9ddbff91
MariyqZlateva <mimitooo4@gmail.com>
2024-08-26 15:51:14 +0300

    adding MapStruct

---
+++ spring-6-reactive/src/main/java/com/zlateva/spring6reactive/mappers/BeerMapper.java
@@ -0,0 +1,12 @@
+package com.zlateva.spring6reactive.mappers;
+
+import com.zlateva.spring6reactive.domain.Beer;
+import com.zlateva.spring6reactive.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+    Beer beerDtoToBeer(BeerDTO dto);
+
+    BeerDTO beerToBeerDto(Beer beer);
+}
