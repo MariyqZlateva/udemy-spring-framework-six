b8e0fcd4b6aa52a5d517bcd836e62909e51ae84f
MariyqZlateva <mimitooo4@gmail.com>
2024-08-27 11:55:34 +0300

    adding post for create new entity

--- spring-6-reactive/src/main/java/com/zlateva/spring6reactive/controllers/BeerController.java
+++ spring-6-reactive/src/main/java/com/zlateva/spring6reactive/controllers/BeerController.java
@@ -3,9 +3,11 @@ package com.zlateva.spring6reactive.controllers;
 import com.zlateva.spring6reactive.model.BeerDTO;
 import com.zlateva.spring6reactive.services.BeerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -13,18 +15,27 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class BeerController {
     public static final String BEER_PATH = "/api/v2/beer";
-    public  static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
+    public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
 
 
     private final BeerService beerService;
 
+    Mono<ResponseEntity<Void>> createNewBeer(BeerDTO beerDTO) {
+        return beerService.saveNewBeer(beerDTO)
+                .map(savedDto -> ResponseEntity.created(UriComponentsBuilder
+                                .fromHttpUrl("http://localhost:8080/" + BEER_PATH
+                                        + "/" + savedDto.getId())
+                                .build().toUri())
+                        .build());
+    }
+
     @GetMapping(BEER_PATH_ID)
-    Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId){
+    Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId) {
         return beerService.getBeerById(beerId);
     }
 
     @GetMapping(BEER_PATH)
-    Flux<BeerDTO> listBeers(){
+    Flux<BeerDTO> listBeers() {
         return beerService.listBeers();
     }
 }
