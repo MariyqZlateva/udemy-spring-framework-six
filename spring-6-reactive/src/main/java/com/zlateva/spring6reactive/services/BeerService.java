8bc5aebb2015222d2c3f753ffc905278f1aa3ef6
MariyqZlateva <mimitooo4@gmail.com>
2024-08-27 11:40:23 +0300

    adding path variable example

--- spring-6-reactive/src/main/java/com/zlateva/spring6reactive/services/BeerService.java
+++ spring-6-reactive/src/main/java/com/zlateva/spring6reactive/services/BeerService.java
@@ -2,8 +2,12 @@ package com.zlateva.spring6reactive.services;
 
 import com.zlateva.spring6reactive.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface BeerService {
 
     Flux<BeerDTO> listBeers();
+
+    Mono<BeerDTO> getBeerById(Integer beerId);
+
 }
