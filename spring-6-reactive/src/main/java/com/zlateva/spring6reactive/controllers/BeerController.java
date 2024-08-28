074a2b1810148d74ed064101d8a86cd745da2f31
MariyqZlateva <mimitooo4@gmail.com>
2024-08-28 14:25:59 +0300

    adding post annotation

--- spring-6-reactive/src/main/java/com/zlateva/spring6reactive/controllers/BeerController.java
+++ spring-6-reactive/src/main/java/com/zlateva/spring6reactive/controllers/BeerController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
@@ -19,6 +20,8 @@ public class BeerController {
 
 
     private final BeerService beerService;
+
+    @PostMapping(BEER_PATH)
 
     Mono<ResponseEntity<Void>> createNewBeer(BeerDTO beerDTO) {
         return beerService.saveNewBeer(beerDTO)
