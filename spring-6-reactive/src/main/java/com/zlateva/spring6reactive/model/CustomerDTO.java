dd9a2f62c7334dda45884fe680abcb77f768795e
MariyqZlateva <mimitooo4@gmail.com>
2024-08-29 14:11:02 +0300

    adding validation on customer

--- spring-6-reactive/src/main/java/com/zlateva/spring6reactive/model/CustomerDTO.java
+++ spring-6-reactive/src/main/java/com/zlateva/spring6reactive/model/CustomerDTO.java
@@ -1,5 +1,7 @@
 package com.zlateva.spring6reactive.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +16,8 @@ import java.time.LocalDateTime;
 public class CustomerDTO {
 
     private Integer id;
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String customerName;
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
