6a570490ac1a2354019957447515587751ad7a3b
MariyqZlateva <mimitooo4@gmail.com>
2024-09-04 16:54:06 +0300

    adding repos and service

---
+++ spring-6-reactive-mongo/src/test/java/com/zlateva/reactivemongo/services/BeerServiceImplTest.java
@@ -0,0 +1,53 @@
+package com.zlateva.reactivemongo.services;
+
+import com.zlateva.reactivemongo.domain.Beer;
+import com.zlateva.reactivemongo.mappers.BeerMapper;
+import com.zlateva.reactivemongo.model.BeerDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+
+
+@SpringBootTest
+class BeerServiceImplTest {
+
+    @Autowired
+    BeerService beerService;
+
+    @Autowired
+    BeerMapper beerMapper;
+
+    BeerDTO beerDTO;
+
+    @BeforeEach
+    void setUp() {
+        beerDTO = beerMapper.beerToBeerDto(getTestBeer());
+    }
+
+    @Test
+    void saveBeer() throws InterruptedException {
+
+        Mono<BeerDTO> savedMono = beerService.saveBeer(Mono.just(beerDTO));
+
+        savedMono.subscribe(savedDto -> {
+            System.out.println("Saved beer DTO: " + savedDto.toString());
+        });
+
+        Thread.sleep(1000l);
+    }
+
+    public static Beer getTestBeer() {
+        return Beer.builder()
+                .beerName("Space Dust")
+                .beerStyle("IPA")
+                .price(BigDecimal.TEN)
+                .quantityOnHand(12)
+                .upc("123213")
+                .build();
+    }
+
+}
