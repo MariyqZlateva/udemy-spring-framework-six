23d3b06d968e6a18bbb2c2dd9ad04ebedbbfc19c
MariyqZlateva <mimitooo4@gmail.com>
2024-09-04 16:09:33 +0300

    adding mongo credentials

--- spring-6-reactive-mongo/src/main/java/com/zlateva/reactivemongo/config/MongoConfig.java
+++ spring-6-reactive-mongo/src/main/java/com/zlateva/reactivemongo/config/MongoConfig.java
@@ -1,10 +1,15 @@
 package com.zlateva.reactivemongo.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
@@ -18,5 +23,15 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     protected String getDatabaseName() {
         return "sfg";
     }
-    
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.createCredential("root",
+                "admin", "example".toCharArray()))
+                .applyToClusterSettings(settings->{
+                    settings.hosts((singletonList(
+                            new ServerAddress("localhost", 27017)
+                    )));
+                });
+    }
 }
